f59498c24a21cb1787c71b92fda237f513eb2f3d
Brandon Laroche <brandon.lrch@gmail.com>
2022-11-26 19:50:29 +0100

    post + update + delete fonctionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.home, name='home'),
+    path('update_task/<str:id>/', views.updateTask, name='update_task'),
+    path('delete_task/<str:id>/', views.deleteTask, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
 
-def hello(request):
-    return HttpResponse('<h1>Hello World !</h1>')
+def home(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'tasks':tasks, 'form':form}
+
+    return render(request, 'list.html', context)
+
+def updateTask(request, id):
+    task = Task.objects.get(id=id)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+
+    return render(request, 'update_task.html', context)
+
+def deleteTask(request, id):
+    task = Task.objects.get(id=id)
+
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+
+    context = {'task':task}
+
+    return render(request, 'delete_task.html', context)
+
