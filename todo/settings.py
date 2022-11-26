65223883948c0e90188d25bb77556639b29c6779
Brandon Laroche <brandon.lrch@gmail.com>
2022-11-26 17:05:56 +0100

    postgresql db connected + first view

--- todo/settings.py
+++ todo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': 'todo_user',
+        'PASSWORD': 'todo12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
